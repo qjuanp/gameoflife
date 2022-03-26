1e1c5ee7bddc9ffcf700f64d65bd0452f242cc27
Juan P <code@qjuanp.dev>
2022-03-26 19:41:22 +0000

    Printing board, no testing added, view directly in the command line

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
 	fmt.Println("Project setup done")
+	board := ramdomInitialization(5, 5, time.Now().UnixNano())
+	printBoard(board)
 }
 
 const ALIVE uint8 = 1
+const ALIVE_CHARACTER string = "\u2588"
+
 const DEAD uint8 = 0
+const DEAD_CHARACTER string = "\u0020"
 
 func ramdomInitialization(rows uint32, columns uint32, seed int64) [][]uint8 {
 	var board [][]uint8 = make([][]uint8, rows)
@@ -29,4 +35,21 @@ func ramdomInitialization(rows uint32, columns uint32, seed int64) [][]uint8 {
 	}
 
 	return board
+}
+
+func printBoard(board [][]uint8) {
+	for _, row := range board {
+		for _, cell := range row {
+			fmt.Printf("%s", toCharacter(cell))
+		}
+		fmt.Println()
+	}
+}
+
+func toCharacter(bit uint8) string {
+	if bit == ALIVE {
+		return ALIVE_CHARACTER
+	} else {
+		return DEAD_CHARACTER
+	}
 }
