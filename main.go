f0ac2e536285e0b27fbbaccbf40e2a5a9867576e
Juan P <code@qjuanp.dev>
2024-02-25 15:33:23 +0000

    Modularization working, more work pending

--- main.go
+++ main.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"os"
 	"os/exec"
-	"strings"
 	"time"
 )
 
 func main() {
 	fmt.Println("Project setup done")
-	currentBoard := ramdomInitialization(100, 100, 1)
+	game := NewGameOfSize(100, 100)
 
 	clear()
 	for {
-		fmt.Print(boardToString(currentBoard, toCharacter))
+		fmt.Print(game.toString())
 		time.Sleep(time.Second)
-		currentBoard = nextBoardState(currentBoard)
+		game = game.next()
 	}
 }
 
@@ -25,135 +23,4 @@ func clear() {
 	c := exec.Command("clear")
 	c.Stdout = os.Stdout
 	c.Run()
-}
-
-type serializer func(uint8) string
-
-const ALIVE uint8 = 1
-const ALIVE_CHARACTER string = "\u2588"
-
-const DEAD uint8 = 0
-const DEAD_CHARACTER string = "\u0020"
-
-func createBoard(rows int, columns int) [][]uint8 {
-	var board [][]uint8 = make([][]uint8, rows)
-
-	for row := range board {
-		board[row] = make([]uint8, columns)
-	}
-
-	return board
-}
-
-func ramdomInitialization(rows uint32, columns uint32, seed int64) [][]uint8 {
-	var board [][]uint8 = make([][]uint8, rows)
-
-	random := rand.New(rand.NewSource(seed))
-
-	for row := range board {
-		board[row] = make([]uint8, columns)
-		for column := range board[row] {
-			randomValue := random.Float64()
-			if randomValue >= 0.5 {
-				board[row][column] = ALIVE
-			} else {
-				board[row][column] = DEAD
-			}
-		}
-	}
-
-	return board
-}
-
-func boardToString(board [][]uint8, serialize serializer) string {
-	boardStringBuilder := strings.Builder{}
-	for _, row := range board {
-		for _, cell := range row {
-			boardStringBuilder.WriteString(fmt.Sprintf("%s", serialize(cell)))
-		}
-		boardStringBuilder.WriteString(fmt.Sprintln())
-	}
-
-	return boardStringBuilder.String()
-}
-
-func toCharacter(bit uint8) string {
-	if bit == ALIVE {
-		return ALIVE_CHARACTER
-	} else {
-		return DEAD_CHARACTER
-	}
-}
-
-func toInt(bit uint8) string {
-	return fmt.Sprintf("%d", bit)
-}
-
-func nextBoardState(currentBoard [][]uint8) [][]uint8 {
-	nextState := createBoard(len(currentBoard), len(currentBoard[0]))
-	for rowIndex, row := range currentBoard {
-		for columnIndex, cell := range row {
-			aliveNeighbors := countAliveNeighboards(currentBoard, rowIndex, columnIndex)
-			nextState[rowIndex][columnIndex] = newCellState(cell, aliveNeighbors)
-		}
-	}
-	return nextState
-}
-
-func countAliveNeighboards(currentBoard [][]uint8, row int, column int) uint8 {
-	aliveNeighbors := 0
-
-	for r := lowerBound(row); r <= upperBound(row, len(currentBoard)); r++ {
-		for c := lowerBound(column); c <= upperBound(column, len(currentBoard[r])); c++ {
-			// fmt.Printf("on(%d,%d)=%d", r, c, currentBoard[r][c])
-			// fmt.Println()
-			if (r != row || c != column) && currentBoard[r][c] == ALIVE {
-				// fmt.Printf("Counted on(%d,%d)=%d", r, c, currentBoard[r][c])
-				// fmt.Println()
-				aliveNeighbors++
-			}
-		}
-	}
-
-	return uint8(aliveNeighbors)
-}
-
-func lowerBound(index int) int {
-	if index == 0 {
-		return 0
-	} else {
-		return index - 1
-	}
-}
-
-func upperBound(index int, maxLength int) int {
-	if index >= (maxLength - 1) {
-		return index
-	} else {
-		return index + 1
-	}
-}
-
-func newCellState(cell uint8, quantityOfAliveNeighbors uint8) uint8 {
-	// Overpopulation
-	if quantityOfAliveNeighbors > 3 {
-		return DEAD
-	}
-
-	// Revive
-	if quantityOfAliveNeighbors == 3 {
-		return ALIVE
-	}
-
-	// Right conditions
-	if cell == ALIVE && quantityOfAliveNeighbors >= 2 && quantityOfAliveNeighbors <= 3 {
-		return ALIVE
-	}
-
-	if quantityOfAliveNeighbors <= 1 {
-		return DEAD
-	}
-
-	// no rule applied
-	return cell
 }
