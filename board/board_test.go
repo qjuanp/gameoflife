f130f8e9a22bc54521cd35793630f5fc888c8d5b
Juan P <code@qjuanp.dev>
2024-03-04 14:24:46 +0000

    Splited tests

---
+++ board/board_test.go
@@ -0,0 +1,126 @@
+package board
+
+import (
+	"reflect"
+	"testing"
+)
+
+func detailedErrorResult(t *testing.T, boardResult Board, boardExpected Board) {
+	t.Errorf("Board result:\n%s\n", boardResult.ToString())
+	t.Errorf("Board expected:\n%s\n", boardExpected.ToString())
+
+	for rowIndex, row := range boardResult {
+		for columnIndex, cell := range row {
+			if cell != boardExpected[rowIndex][columnIndex] {
+				t.Errorf("At(row=%d,column%d) Value=%t | Expected=%t", rowIndex, columnIndex, cell, boardExpected[rowIndex][columnIndex])
+			}
+		}
+	}
+}
+
+func TestCreateBoard(t *testing.T) {
+	const columnsExpected uint = 5
+	const rowsExpected uint = 5
+	boardExpected := Board{
+		{DEAD, DEAD, DEAD, DEAD, DEAD},
+		{DEAD, DEAD, DEAD, DEAD, DEAD},
+		{DEAD, DEAD, DEAD, DEAD, DEAD},
+		{DEAD, DEAD, DEAD, DEAD, DEAD},
+		{DEAD, DEAD, DEAD, DEAD, DEAD},
+	}
+
+	boardResult := NewEmptyBoardOfSize(rowsExpected, columnsExpected)
+	if !reflect.DeepEqual(boardResult, boardExpected) {
+		t.Errorf("Unexpected created board")
+		detailedErrorResult(t, boardResult, boardExpected)
+	}
+}
+
+func TestRandomInitializationSize(t *testing.T) {
+	const columnsExpected uint = 5
+	const rowsExpected uint = 5
+	seed := int64(1)
+
+	boardResult := NewRandomBoard(rowsExpected, columnsExpected, seed)
+	rowsResult := boardResult.NumberOfColumns()
+	columnsResult := boardResult.NumberOfRows()
+
+	if rowsResult != rowsExpected {
+		t.Errorf("Rows: %d, wanted %d", rowsResult, rowsExpected)
+	}
+
+	if columnsResult != columnsExpected {
+		t.Errorf("Columns: %d, wanted %d", columnsResult, columnsExpected)
+	}
+}
+
+func TestRandomInitializationBoard(t *testing.T) {
+	const (
+		columnsExpected uint = 5
+		rowsExpected    uint = 5
+	)
+	seed := int64(1)
+	boardExpected := Board{
+		{ALIVE, ALIVE, ALIVE, DEAD, DEAD},
+		{ALIVE, DEAD, DEAD, DEAD, DEAD},
+		{ALIVE, ALIVE, DEAD, DEAD, DEAD},
+		{DEAD, DEAD, DEAD, ALIVE, DEAD},
+		{DEAD, DEAD, ALIVE, ALIVE, DEAD},
+	}
+
+	boardResult := NewRandomBoard(rowsExpected, columnsExpected, seed)
+
+	if !reflect.DeepEqual(boardResult, boardExpected) {
+		t.Errorf("Unexpected initialized board")
+		detailedErrorResult(t, boardResult, boardExpected)
+	}
+}
+
+// func TestLowerBoundCentreCell(t *testing.T) {
+// 	position := 3
+// 	expectedLowerBound := 2
+
+// 	result := lowerBound(position)
+
+// 	if result != expectedLowerBound {
+// 		t.Errorf("Lower bound mismatch result=%d | expected=%d", result, expectedLowerBound)
+// 	}
+// }
+
+// func TestLowerBoundCornerCell(t *testing.T) {
+// 	position := DEAD
+// 	expectedLowerBound := DEAD
+
+// 	result := lowerBound(position)
+
+// 	if result != expectedLowerBound {
+// 		t.Errorf("Lower bound mismatch result=%d | expected=%d", result, expectedLowerBound)
+// 	}
+// }
+
+// func TestUpperBoundCentreCell(t *testing.T) {
+// 	position := 3
+// 	length := 5
+// 	expectedLowerBound := 4
+
+// 	result := upperBound(position, length)
+
+// 	if result != expectedLowerBound {
+// 		t.Errorf("Lower bound mismatch result=%d | expected=%d", result, expectedLowerBound)
+// 	}
+// }
+
+// func TestUpperBoundCornerCell(t *testing.T) {
+// 	position := 4
+// 	length := 5
+// 	expectedLowerBound := 4
+
+// 	result := upperBound(position, length)
+
+// 	if result != expectedLowerBound {
+// 		t.Errorf("Lower bound mismatch result=%d | expected=%d", result, expectedLowerBound)
+// 	}
+// }
+
+
+
